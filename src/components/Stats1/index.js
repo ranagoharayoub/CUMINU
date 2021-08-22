ee02df8f4826c130733c219d0938a3852940de93
ranagoharayoub <ranagohar1163@gmail.com>
2021-08-22 15:12:02 +0500

    two section added

--- src/components/Stats1/index.js
+++ src/components/Stats1/index.js
@@ -1,67 +1,100 @@
 import React from 'react'
 import styles from "./style.module.css";
+import TypeWritter from "typewriter-effect";
+import chart from '../../assets/images/icon.png'
 
 function Stats1() {
     return (
         <div className={styles.main}>
+            <div className='py-10 px-8 w-full bg-gray-900 md:px-24 text-left xxl:px-4 xxl:max-w-9xl xxl:mx-auto'>
 
-            <div className={styles.upper}>
+                <div className={styles.upper}>
 
-                <div className={styles.titlePrice}>
+                    <div className={styles.titlePrice}>
 
-                    <div className={styles.title}>
-                    LATEST PRICE
+                        <div className={styles.title}>
+                        LATEST PRICE
+                        </div>
+                        
+                        <div className={styles.price}>
+                        $1.985
+                        </div>
+
                     </div>
+
+                    <div className={styles.titlePrice}>
+
+                        <div className={styles.title}>
+                        TOTAL SUPPLY
+                        </div>
+
+                        <div className={styles.price}>
+                        5.29B
+                        </div>
+
+                    </div>
+
+                    <div className={styles.titlePrice}>
+
+                        <div className={styles.title}>
+                        MARKET CAP
+                        </div>
+
+                        <div className={styles.price}>
+                        $10.5B
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className={styles.middle}>
+
+                    <div className={styles.btn}>
+                        <img src={chart} alt='img'></img>
+                        <div>LIVE CHART</div>
+                    </div>
+                    <div className={styles.btn}>
+                        <img src={chart} alt='img'></img>
+                        <div>ETHERSCAN</div>
+                    </div>
+
+                </div>
+
+                <p className={styles.bottom}>
+                    <span style={{display:'flex', alignItems:'baseline'}}>
+                        A total of{" "}
+                        <span
+                    className="block text-pink-800"
+                    style={{
+                        fontFamily: "helvetica",
+                        fontWeight: "normal",
+                        fontStyle: "normal",
+                        fontSize:"5vmax",
+                        marginLeft:"1vmax"
+                    }}
+                    >
+                    <TypeWritter
+                        options={{
+                        strings: ["4 , 7000 , 000 , 000"],
+                        autoStart: true,
+                        loop: true,
+                        }}
+                    />
+                        
+                        </span>    
+                    </span>
+
+                    <span>tokens have been 
                     
-                    <div className={styles.price}>
-                    $1.985
-                    </div>
+                        <span style={{color:"white"}}>{" "}permanently removed {" "}</span>
 
-                </div>
-
-                <div className={styles.titlePrice}>
-
-                    <div className={styles.title}>
-                    TOTAL SUPPLY
-                    </div>
-
-                    <div className={styles.price}>
-                    5.29B
-                    </div>
-
-                </div>
-
-                <div className={styles.titlePrice}>
-
-                    <div className={styles.title}>
-                    MARKET CAP
-                    </div>
-
-                    <div className={styles.price}>
-                    $10.5B
-                    </div>
-
-                </div>
+                        from existence.
+                    </span>
+                    <span>They no longer exist in any wallet, dead or otherwise</span> 
+                </p>
 
             </div>
-
-            <div className={styles.middle}>
-
-                <div className={styles.btn}>
-                    <img src={null} alt='img'></img>
-                    <div>LIVE CHART</div>
-                </div>
-                <div className={styles.btn}>
-                    <img src={null} alt='img'></img>
-                    <div>ETHERSCAN</div>
-                </div>
-
-            </div>
-
-            <div className={styles.bottom}>
-                A total of tokens have been permanently removed from existence. They no longer exist in any wallet, dead or otherwise.
-            </div>
-
         </div>
     )
 }
--- src/components/Tokenomics/index.js
+++ src/components/Tokenomics/index.js
@@ -1,33 +1,60 @@
 import React from 'react'
 import styles from './style.module.css'
+import doubleCircle from '../../assets/images/Group_70.png'
+import TypeWritter from "typewriter-effect";
+
 function Tokenomics() {
     return (
         <div className={styles.main}>
+            <div className='py-10 px-8 w-full bg-gray-900 md:px-24 text-left xxl:px-4 xxl:max-w-9xl xxl:mx-auto'>
+                <div className={styles.designTitle}>
 
-            <div className={styles.designTitle}>
-
-                <div className={styles.design}>
-                    <img src={null} alt='circles'></img>
-                </div>
-                <div className={styles.Title}>
-                    Dual-Chain Tokenomics
-                </div>
-                
-            </div>
-
-            <div>
-                We have developed a first-of-its-kind cross-chain burn mechanism that utilizes both the Ethereum and Binance blockchains.
-            </div>
-
-            <div className={styles.btn}>
-
-                <div className={styles.btn1}>
-                    READ MORE
-                </div>
-                <div className={styles.btn2}>
-                    ABOUT TIPINU
+                    <div className={styles.design}>
+                        <img src={doubleCircle} alt='circles'></img>
+                    </div>
+                    <div className={styles.Title}>
+                        Dual-Chain<br></br> Tokenomics
+                    </div>
+                    
                 </div>
 
+                <div className={styles.text}>
+                    We have developed a first-of-its-kind
+                    
+                    <span
+                    className="block text-pink-800"
+                    style={{
+                        fontFamily: "helvetica",
+                        fontWeight: "normal",
+                        fontStyle: "normal",
+                        fontSize:"5vmax",
+                        
+                    }}
+                    >
+                    <TypeWritter
+                        options={{
+                        strings: ["cross-chain burn mechanism"],
+                        autoStart: true,
+                        loop: true,
+                        }}
+                    />
+                    </span>
+                    that utilizes both the
+                    <br></br> 
+                    Ethereum and Binance blockchains.
+                   
+                </div>
+
+                <div className={styles.btn}>
+
+                    <div className={styles.btn1}>
+                        READ MORE
+                    </div>
+                    <div className={styles.btn2}>
+                        ABOUT TIPINU
+                    </div>
+
+                </div>
             </div>
         </div>
     )
